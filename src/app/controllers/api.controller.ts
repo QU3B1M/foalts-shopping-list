8e2ba8f33267752b40c4d42b42d434bfa178263b
QU3B1M <kevinledesmam95@gmail.com>
2022-04-07 16:20:54 -0300

    OpenAPI implemented

--- src/app/controllers/api.controller.ts
+++ src/app/controllers/api.controller.ts
@@ -1,7 +1,15 @@
-import { Context, controller, Get, HttpResponseOK } from "@foal/core";
+import { ApiInfo, ApiServer, controller } from "@foal/core";
 import { ItemController } from "./item.controller";
 import { ListController } from "./list.controller";
 
+
+@ApiInfo({
+  title: 'Application API',
+  version: '1.0.0'
+})
+@ApiServer({
+  url: '/api'
+})
 export class ApiController {
 	subControllers = [
     controller("/items", ItemController), 
--- src/app/controllers/index.ts
+++ src/app/controllers/index.ts
@@ -1,3 +1,4 @@
 export { ApiController } from './api.controller';
 export { ItemController } from './item.controller';
 export { ListController } from './list.controller';
+export { OpenapiController } from './openapi.controller';
